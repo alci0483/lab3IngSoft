19e01c8b5779915115e4b74385bb0c82b5e73160
alci0483 <alci0483@gmail.com>
2023-10-18 19:15:31 -0300

    se termino todo el laboratorio!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :monsters, only: [:index, :show]
 
+  delete "monsters/:id", to: "monsters#destroy", as: :destroy_monster
+
   root "monsters#index"
 end
